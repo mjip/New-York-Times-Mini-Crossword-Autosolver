594921fb8602f07f526f281998e8be53fe515a3e
mjip <marie.payne@hotmail.ca>
2019-01-04 22:55:23 -0500

    objectified

--- Main.py
+++ Main.py
@@ -9,31 +9,36 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 
-def main():
-	chrome_options = Options()
-	chrome_options.add_argument("--headless")
-	chrome_options.add_argument("--window-size=1024x1400")
+def CrosswordAI(Object):
+	def __init__(self):
+		chrome_options = Options()
+		chrome_options.add_argument("--headless")
+		chrome_options.add_argument("--window-size=1024x1400")
 
-	if not os.path.isfile('chromedriver'):
-		zipresp = urllib.request.urlopen('https://chromedriver.storage.googleapis.com/2.45/chromedriver_linux64.zip')
-		with open('chromedriver_linux64.zip', 'wb') as tempzip:
-			tempzip.write(zipresp.read())
+		if not os.path.isfile('chromedriver'):
+			zipresp = urllib.request.urlopen('https://chromedriver.storage.googleapis.com/2.45/chromedriver_linux64.zip')
+			with open('chromedriver_linux64.zip', 'wb') as tempzip:
+				tempzip.write(zipresp.read())
 
-		with zipfile.ZipFile('chromedriver_linux64.zip') as zip_ref:
-			zip_ref.extractall()
-	os.chmod('chromedriver', 0o755)
+			with zipfile.ZipFile('chromedriver_linux64.zip') as zip_ref:
+				zip_ref.extractall()
+		os.chmod('chromedriver', 0o755)
+	
+		chrome_driver = os.path.join(os.getcwd(), 'chromedriver')
 
-	chrome_driver = os.path.join(os.getcwd(), 'chromedriver')
+		self.driver = webdriver.Chrome(chrome_options=chrome_options, executable_path=chrome_driver)
+	def start_game(self):
+		self.driver.get('https://www.nytimes.com/crosswords/game/mini')
+		elem = self.driver.find_element_by_tag_name('body')
+		elem.send_keys(Keys.RETURN)
 
-	driver = webdriver.Chrome(chrome_options=chrome_options, executable_path=chrome_driver)
-	driver.get('https://www.nytimes.com/crosswords/game/mini')
-	elem = driver.find_element_by_tag_name('body')
-	elem.send_keys(Keys.RETURN)
-
-	driver.get_screenshot_as_file('minicrossword.png')
-	driver.close()
+	def end_game(self):
+		self.driver.get_screenshot_as_file('minicrossword.png')
+		self.driver.close()
 
 
 if __name__ == '__main__' :
-	main()
+	ai = CrosswordAI()
+	ai.start_game()
+	ai.end_game()
 
